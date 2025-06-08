fca1f608f57096e35673ab925d51aab23931febf
pasya4612 <pasyavaresifernanda@gmail.com>
2025-06-08 18:34:03 +0700

    Update A04_ProgramKomnum_3.py

--- A04_ProgramKomnum_3.py
+++ A04_ProgramKomnum_3.py
@@ -4,16 +4,16 @@ def f(x):
 def posisi_salah(a, b, xr_true, min_error = 0, max_error = 1, max_iter=100):
     if f(a) * f(b) >= 0:
         return None
-
+ 
     for i in range(max_iter):
         xr = b - (f(b) * (a - b)) / (f(a) - f(b))
         fxr = f(xr)
 
         if i == 0:
-            error_true = None
+            error_true = None 
         else:
             error_true = abs((xr - xr_true) / xr)
-
+        
         if error_true is not None:
             print(f"Iterasi {i}: a={a:.2f}, b={b:.2f}, xr={xr:.2f}, f(xr)={fxr:.2f}, error_true={error_true:.2f}")
         else:
@@ -29,7 +29,7 @@ def posisi_salah(a, b, xr_true, min_error = 0, max_error = 1, max_iter=100):
 
     return xr
 
-a = -5
+a = -5   
 b = 8
 xr_true = 4
 result = posisi_salah(a, b, xr_true)
